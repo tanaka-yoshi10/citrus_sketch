19c02ea6c7db3e55a57e15441468abc4069579f8
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2018-04-30 14:42:47 +0900

    VisualStudio code と ファーム2.19に対応

--- main.rb
+++ main.rb
@@ -6,21 +6,21 @@ def executeCommand(command)
   when "q"
     Sys.fileload()
   when "t"
-    l = I2c.request(@APTemp, 2)
+    l = new.request(@APTemp, 2)
     #Serial.println(0, l.to_s)
 
-    s1 = I2c.lread()
+    #s1 = I2c.lread()
     #Serial.println(0, s1.inspect)
-    s2 = I2c.lread()
+    #s2 = I2c.lread()
     #Serial.println(0, s2.inspect)
     temp_h, temp_l = s1, s2
     temp = ((temp_h << 4) + (temp_l >> 4)) * 0.0625
 
     t = myRound(temp)
-    Serial.println(0, t)
-    Serial.println(1, t)
+    @serial0.println(0, t)
+    @serial1.println(1, t)
   when "l"
-    Serial.println(0, @sw.to_s)
+    @serial0.println(0, @sw.to_s)
   when "1"
     @sw = 1
     Servo.write(0, 0)
@@ -36,25 +36,25 @@ def myRound(value)
   str[0..-2] + "." + str[-1]
 end
 
-Serial.begin(0, 9600)
-Serial.begin(1, 9600)
+@serial0 = Serial.new(0, 9600)
+@serial1 = Serial.new(1, 9600)
 
 @sw = 0
 
 Servo.attach(0 , 9)
 Servo.write(0, 10)
 
-I2c.sdascl(17,16)
+# I2c.sdascl(17,16)
 delay(300)
 
 loop do
-  while(Serial.available(0) > 0) do
-    c = Serial.read(0).chr
+  while(@serial0.available(0) > 0) do
+    c = @serial0.read(0).chr
     executeCommand(c)
   end
 
-  while(Serial.available(1) > 0) do
-    c = Serial.read(1).chr
+  while(@serial1.available(1) > 0) do
+    c = @serial1.read(1)
     executeCommand(c)
   end
 
